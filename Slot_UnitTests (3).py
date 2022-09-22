3b20af0acdd811ce16fcb9cb7221ec704b1c8a7a
logansimpson01 <113566378+logansimpson01@users.noreply.github.com>
2022-09-21 19:24:06 -0500

    Update Slot_UnitTests (3).py

--- Slot_UnitTests (3).py
+++ Slot_UnitTests (3).py
@@ -1,4 +1,4 @@
-import lab05 as lab05
+import lab04 as lab04
 import unittest
 from unittest.mock import patch
 from io import StringIO
@@ -10,7 +10,7 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["YeS"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.play_again()
+                results = lab04.play_again()
                 self.assertTrue(results, "When input is yes the function should return True")
 
     def test_play_again_returns_False_with_N_or_NO(self):
@@ -18,7 +18,7 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["nO"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.play_again()
+                results = lab04.play_again()
                 self.assertFalse(results, "When input is No play_again should return False")
 
     def test_play_again_warns_user_with_bad_input(self):
@@ -26,7 +26,7 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["incorrect", "nO"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.play_again()
+                results = lab04.play_again()
                 self.assertFalse(results, "When input is no the play_again should return False")
                 l = es
         self.assertIn("try again", l.getvalue().lower(), "Bad input should be followed with an error with at least Please Try again in it.")
@@ -37,7 +37,7 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["chiefs", "incorrect", "nO"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.play_again()
+                results = lab04.play_again()
                 self.assertFalse(results, "play_again should return False when given 2 incorrect inputs and then no")
 
     def test_get_wager_returns_wager_amount(self):
@@ -45,7 +45,7 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["30"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.get_wager(200)
+                results = lab04.get_wager(200)
                 self.assertEqual(30, results, "getWager should return 30 when the user enters 30")
 
 
@@ -54,7 +54,7 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["-5", "45"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.get_wager(200)
+                results = lab04.get_wager(200)
                 self.assertEqual(45, results, "getWager should return 45 when the user enters -5, 30")
 
     def test_get_wager_returns_wager_amount_after_negative(self):
@@ -62,7 +62,7 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["-5", "45"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.get_wager(200)
+                results = lab04.get_wager(200)
                 self.assertEqual(45, results, "getWager should return 45 when the user enters -5, 30")
                 l = es
 
@@ -71,14 +71,14 @@ class TestLabFunctions(unittest.TestCase):
         user_input = ["105", "22"]
         with patch('sys.stdout', new_callable=StringIO) as es:
             with patch('builtins.input', side_effect=user_input):
-                results = lab05.get_wager(30)
+                results = lab04.get_wager(30)
                 self.assertEqual(22, results, "getWager should return 22 when the user enters 105, 22, and the bank is less than 105")
                 l = es
 
 
     def test_get_slot_results_returns_a_tuple_of_integers(self):
 
-        results = lab05.get_slot_results()
+        results = lab04.get_slot_results()
         self.assertIsInstance(results, tuple, "get_slot_results should return a tuple")
         self.assertEqual(3, len(results), "get_slot_results shoudl return 3 items")
         self.assertIsInstance(results[0], int, "Item at index 0 was not an integer")
@@ -88,27 +88,27 @@ class TestLabFunctions(unittest.TestCase):
 
     def test_get_matches_returns_correct_number_of_matches(self):
 
-        results = lab05.get_matches(3, 3, 3)
+        results = lab04.get_matches(3, 3, 3)
         self.assertEqual(3, results, "get_matches returns 3 when given 3 matching values")
-        results = lab05.get_matches(4, 4, 4)
+        results = lab04.get_matches(4, 4, 4)
         self.assertEqual(3, results, "get_matches returns 3 when given 3 matching values")
-        results = lab05.get_matches(4, 4, 5)
+        results = lab04.get_matches(4, 4, 5)
         self.assertEqual(2, results, "get_matches returns 2 when given 2 matching values")
-        results = lab05.get_matches(8, 4, 4)
+        results = lab04.get_matches(8, 4, 4)
         self.assertEqual(2, results, "get_matches returns 2 when given 2 matching values")
-        results = lab05.get_matches(8, 4, 8)
+        results = lab04.get_matches(8, 4, 8)
         self.assertEqual(2, results, "get_matches returns 2 when given 2 matching values")
-        results = lab05.get_matches(8, 4, 3)
+        results = lab04.get_matches(8, 4, 3)
         self.assertEqual(0, results, "get_matches returns 0 when given 0 matching values")
 
 
     def test_get_payout_returns_correct_results(self):
 
-        results = lab05.get_payout(3, 0)
+        results = lab04.get_payout(3, 0)
         self.assertEqual(-3, results, "get_payout returns -3 when given 3 and no matches.")
-        results = lab05.get_payout(2, 3)
+        results = lab04.get_payout(2, 3)
         self.assertEqual(18, results, "get_payout returns 8 when given 2 and 3 matches.")
-        results = lab05.get_payout(5, 2)
+        results = lab04.get_payout(5, 2)
         self.assertEqual(10, results, "get_payout returns 10 when given 5 and 2 matches.")
 
 
